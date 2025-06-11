ccab3c59215d3295d88792a40cd7e330a9135b7a
kendell pierre <kpierre24@gmail.com>
2025-06-11 22:52:00 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,9 @@
 
+
 import type { User, Course, Lesson, Assignment, Submission, Payment, AttendanceRecord, NotificationMessage, Enrollment, QuizQuestion, RubricCriterion, Announcement } from '@/types';
 import { UserRole, AssignmentType, QuestionType, AttendanceStatus } from '@/types';
 
-export const APP_NAME = "School of Ministry";
+export const APP_NAME = "students in bible school";
 
 export const SAMPLE_SUPER_ADMIN: User = {
   id: 'user-super-admin',
